e6610c0c26171fc7e205de2db3114cac7b0f5017
Manuja Demin <156160654+Manuja-D@users.noreply.github.com>
2024-12-21 03:06:18 +0800

    Frontend BG update

--- Frontend/2025newyearapp/app/page.js
+++ Frontend/2025newyearapp/app/page.js
@@ -1,9 +1,12 @@
 import Countdown from "../components/Countdown";
+import SnowEffect from "../components/snowEffect";
 
 export default function Home() {
   return (
     <div>
       <Countdown />
+      <SnowEffect />
     </div>
+
   );
 }
