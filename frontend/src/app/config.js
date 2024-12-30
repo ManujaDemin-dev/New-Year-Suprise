e686c11cb258ff572970f440f578e32f5b29ebac
Ravinath Akalanka <rajaravinath0@gmail.com>
2024-12-30 02:22:10 +0000

    update: add HTTPS

--- frontend/src/app/config.js
+++ frontend/src/app/config.js
@@ -6,12 +6,12 @@ const config = {
   FRONTEND_URL:
     ENVIRONMENT === "DEV"
       ? "http://localhost:3000"
-      : "https://wishcraft.ravinath.dev",
+      : "https://wishcraft.ravinath.online",
 
   BASE_API_URL_DOCKER:
     ENVIRONMENT === "DEV"
       ? "http://localhost:8081/api/v1"
-      : "http://64.227.46.49/api",
+      : "https://64.227.46.49/api/v1",
 };
 
 export default config;
