927dd7a904fc9bea95e36ef4d998e272286b703c
Ravinath Akalanka <rajaravinath0@gmail.com>
2024-12-30 00:44:12 +0000

    change: prod api url

--- frontend/src/app/config.js
+++ frontend/src/app/config.js
@@ -8,7 +8,10 @@ const config = {
       ? "http://localhost:3000"
       : "https://wishcraft.ravinath.dev",
 
-  BASE_API_URL_DOCKER: "http://localhost:8081/api/v1",
+  BASE_API_URL_DOCKER:
+    ENVIRONMENT === "DEV"
+      ? "http://localhost:8081/api/v1"
+      : "http://64.227.46.49/api/v1",
 };
 
 export default config;
